45d5cd6eab68cec76754f2c5c9e532ea83bb80fa
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-07-10 22:53:56 -0300

    frontend -> API integration done | backend -> templates done | Next Step: connect auth.py with HTML

--- autogit.sh
+++ autogit.sh
@@ -1,4 +1,4 @@
 git add -A
-git commit -m "main.py -> done"
+git commit -m "frontend -> API integration done | backend -> templates done | Next Step: connect auth.py with HTML"
 git push -u origin main
 git status
